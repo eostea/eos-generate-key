4043f354ca616d9e496902fe07da76c59dfd3921
iWtbAbh <1483601725@qq.com>
2018-06-18 13:21:39 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,23 @@
     <img src="https://eosfans-static.strahe.com/photo/2018/bc17c8bf-d168-4fdc-b99a-83bea533d752.png?x-oss-process=image/resize,w_1920" style="height:200px;">
 
     <div v-show="show">
+      <h3>随机生成私钥</h3>
       <p><span>私钥：</span>{{ privateKey }}</p>
       <p><span>公钥：</span>{{ publicKey }}</p>
     </div>
-    <hr>
     <div style="margin-top: 40px;">
       <button class="primary" @click="getPrivateKey">一键生成私钥/公钥（随机生成）</button>
       <p>通过一键生成私钥和公钥，<span style="color:orange;">同时请注意保管好自己的私钥</span></p>
     </div>
     <hr>
+    <div v-show="showSeed">
+      <h3>通过助记词生成私钥</h3>
+      <p><span>私钥：</span>{{ privateKeyBySeed }}</p>
+      <p><span>公钥：</span>{{ publicKeyBySeed }}</p>
+    </div>
     <div style="margin-top: 40px;">
       <input type="text" v-model="seed">
-      <button class="primary" @click="getPrivateKeyBySeed">通过助记词，获取私钥（生成固定私钥）</button>
+      <button class="primary" @click="showDialog = true">通过助记词，获取私钥（生成固定私钥）</button>
       <p>可以输入一个固定的字符串，生成一个固定的私钥/公钥。</p>
       <p>助记词: 应该是使用至少128个随机位来产生一个好的私钥，<span style="color:orange;">同时请注意保管好助记词</span></p>
     </div>
@@ -36,6 +41,22 @@
       <p v-if="showPrivateTips"><span>验证EOS私钥：</span>{{ valiPrivateStatus ? '正确' : '错误！'}}</p>
       <p>（验证EOS私钥格式）</p>
     </div>
+
+    <div class="dialog" v-if="showDialog">
+      <div class="dialog-container">
+        <div class="dialog-header">
+          <h2>提示：</h2>
+        </div>
+        <div class="dialog-body">
+          <p>确定使用当前的助记词生成固定的私钥吗？</p>
+          <p>助记词：{{ this.seed }}</p>
+        </div>
+        <div class="dialog-footer">
+          <button @click="showDialog = false">取消</button>
+          <button class="primary" @click="generateKeyBySeed">确定</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -47,18 +68,28 @@ export default {
   data () {
     return {
       show: false,
+      showSeed: false,
       publicKey: '',
       privateKey: '',
+      publicKeyBySeed: '',
+      privateKeyBySeed: '',
       seed: '',
       valiPublicKey: '',
       valiPrivateKey: '',
       valiPublicStatus: false,
       valiPrivateStatus: false,
       showPublicTips: false,
-      showPrivateTips: false
+      showPrivateTips: false,
+
+      // dialog
+      showDialog: false
     }
   },
   methods: {
+    generateKeyBySeed () {
+      this.getPrivateKeyBySeed()
+      this.showDialog = false
+    },
     getPrivateKey () {
       this.show = true
       ecc.randomKey().then(privateKey => {
@@ -67,9 +98,9 @@ export default {
       })
     },
     getPrivateKeyBySeed () {
-      this.show = true
-      this.privateKey = ecc.seedPrivate(this.seed)
-      this.publicKey = ecc.privateToPublic(this.privateKey)
+      this.showSeed = true
+      this.privateKeyBySeed = ecc.seedPrivate(this.seed)
+      this.publicKeyBySeed = ecc.privateToPublic(this.privateKeyBySeed)
     },
     isValidPublic () {
       this.showPublicTips = true
@@ -90,7 +121,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 
 input[type='text'] {
@@ -151,6 +182,26 @@ button {
   padding: 12px 20px;
   font-size: 14px;
   border-radius: 4px;
+}
+.dialog {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+}
+.dialog-container {
+  background: #fff;
+  padding: 30px 40px;
+  border-radius: 2px;
+}
+.dialog-footer {
+  text-align: right;
 }
 
 </style>
