6067da31b78e728eeb91bb2ed0cefe64d245f77d
iWtbAbh <1483601725@qq.com>
2018-06-18 13:30:07 +0800

    fix build file dist -> docs

--- src/App.vue
+++ src/App.vue
@@ -121,9 +121,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 20px;
 }
 body {
+  margin: 0;
   position: relative;
   width: 100%;
   height: 100%;
